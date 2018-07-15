b53f3ef734c5228bf18136c2bbdc86b48eda9534
phasenraum2010 <thomas.woehlke@googlemail.com>
2018-07-16 00:33:22 +0200

    ongoing work on #34 #39

--- defcon-local-neo4j/src/main/java/org/woehlke/humanrights/digital/defense/defcon1/config/WebMvcWebSecurityConfig.java
+++ defcon-local-neo4j/src/main/java/org/woehlke/humanrights/digital/defense/defcon1/config/WebMvcWebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,7 +34,7 @@ import org.woehlke.humanrights.digital.defense.defcon1.config.properties.Applica
 @EnableWebMvc
 @EnableWebSecurity
 @EnableRedisHttpSession
-//@EnableSpringDataWebSupport
+@EnableSpringDataWebSupport
 @EnableConfigurationProperties({
     //AllProperties.class,
     //StorageProperties.class,
