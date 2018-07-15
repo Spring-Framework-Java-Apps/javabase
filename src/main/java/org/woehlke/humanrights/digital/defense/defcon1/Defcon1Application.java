8853e11a32c72345c8ad50b5810e742b383cc752
phasenraum2010 <thomas.woehlke@googlemail.com>
2018-07-15 09:54:23 +0200

    ongoing work on #1, #2 Spring Maven Project Setup

---
+++ src/main/java/org/woehlke/humanrights/digital/defense/defcon1/Defcon1Application.java
@@ -0,0 +1,12 @@
+package org.woehlke.humanrights.digital.defense.defcon1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Defcon1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Defcon1Application.class, args);
+	}
+}
---
+++ src/test/java/org/woehlke/humanrights/digital/defense/defcon1/Defcon1ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.woehlke.humanrights.digital.defense.defcon1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Defcon1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
