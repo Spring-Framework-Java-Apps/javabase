015662fdfe2bf33140116b10c3e1bed429ece04d
phasenraum2010 <thomas.woehlke@googlemail.com>
2018-07-16 07:21:10 +0200

    ongoing work on #34 #39

---
+++ defcon-parent/conf.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+export DEFCON1_LOGIN_USERNAME=nbc
+export DEFCON1_LOGIN_PASSWORD=natural-born-coder
+export DEFCON1_GOOGLE_APS_API_KEY=AIzaSyCMWLD6SK1QY3f-94mDS6x7u_jxwYASgqY
+export DEFCON1_GOOGLE_ANALYTICS_ID=UA-17174370-11
+export DEFCON1_GOOGLE_WEBMASTER_TOOLS_ID=3eksTC3Rvao6fLvSUwoRECBwd9Gdg4F51IhOE3DD-T8
+export DEFCON1_MICROSOFT_WEBMASTER_TOOLS_ID=52F1EDC0244BCDCF5C95E402E9E3E49B
+export DEFCON1_FACEBOOK_APP_ID=1721279777913278
+export DEFCON1_TWITTER_CARD_SITE=port80guru
+export DEFCON1_TWITTER_CARD_CREATOR=port80guru
+
+export DEFCON1_PROFILE_DEFAULT='-Pdefault'
+export DEFCON1_PROFILE_DEV_embedded='-Pdevembedded'
+export DEFCON1_PROFILE_INT_embedded='-Pintembedded'
+export DEFCON1_PROFILE_TEST_embedded='-Ptestembedded'
+export DEFCON1_PROFILE_PROD_embedded='-Pprodembedded'
+export DEFCON1_PROFILE_TRAVIS_embedded='-Ptravisembedded'
+export DEFCON1_PROFILE_DEV='-Pdev'
+export DEFCON1_PROFILE_INT='-Pint'
+export DEFCON1_PROFILE_TEST='-Ptest'
+export DEFCON1_PROFILE_PROD='-Pprod'
+export DEFCON1_PROFILE_TRAVIS='-Ptravis'
+
+export DEFCON1_PROFILE_RUN=$DEFCON1_PROFILE_DEFAULT
+export DEFCON1_PROFILE_RELEASE=$DEFCON1_PROFILE_TEST_embedded
+export DEFCON1_PROFILE_BUILD_JAR=$DEFCON1_PROFILE_DEFAULT
+# TODO
+export DEFCON1_PROFILE_RUN_JAR_LIKE_HEROKU=''
+
+export MAVEN_OPTS="-Xmx2048m"
+export PORT=8080
+#export JAVA_OPTS='-Xmx350m -Xss512k'
+
+export JAVA_OPTIONS='-Xmx2048m -Xss4096k -Dfile.encoding=UTF-8'
+
+export MVN_CMD='../mvnw'
+
+function mvn_fast_build(){
+    $MVN_CMD clean install
+}
+
+function mvn_build(){
+    $MVN_CMD clean install dependency:tree
+}
+
+function mvn_run(){
+    $MVN_CMD $DEFCON1_PROFILE_RUN clean spring-boot:run
+}
+
+function mvn_heruko(){
+    export JAVA_OPTIONS='-Xmx300m -Xss512k -Dfile.encoding=UTF-8'
+    $MVN_CMD $DEFCON1_PROFILE_RUN clean spring-boot:run
+}
+
+function mvn_jbake_help(){
+    $MVN_CMD jbake:help -Ddetail -DgoalName=seed
+}
+
+function run_jbake_seed(){
+    $MVN_CMD jbake:seed
+}
+
+function mvn_jbake_run(){
+    $MVN_CMD clean jbake:generate jbake:inline
+}
+
+function mvn_jbake(){
+    $MVN_CMD clean jbake:generate
+}
+
+function mvn_site(){
+    $MVN_CMD clean site site:deploy
+}
+
+function mvn_site_run(){
+    $MVN_CMD clean site site:run
+}
+
+function mvn_travis(){
+    $MVN_CMD clean jbake:generate site site:deploy -Ptravis -Dtest=AlphaTopLevelSuiteIT  -B -V
+}
---
+++ defcon-parent/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+source conf.sh
+
+export MVN_CMD='./mvnw'
+
+
+#mvn_jbake_help
+#run_jbake_seed
+#mvn_jbake
+
+#mvn_fast_build
+#mvn_build
+mvn_site
+#mvn_travis
+#mvn_heruko
+#mvn_run
+
+exit 0
